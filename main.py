244e44dbe485ac77e3ab02fb448ee99309e52351
K0tlyarski <djgame2.9.6@gmail.com>
2022-10-02 22:31:24 +0400

    Create a function for creating folders

--- main.py
+++ main.py
@@ -3,3 +3,9 @@ import requests
 import lxml
 import os
 from time import sleep
+
+def create_directory(dir_name):
+    if not os.path.isdir('dataset'):
+        os.mkdir('dataset')
+    if not os.path.exists(f'dataset/{dir_name}'):
+        os.mkdir(f'dataset/{dir_name}')
